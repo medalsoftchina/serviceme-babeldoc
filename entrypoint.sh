ae4593ba4b27bf67837db3c0c6c47be8033c2ced
rzq <zhiqi.rui@medalsoft.com>
2025-09-23 13:28:53 +0800

    fix:修改启动脚本

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-NUM_WORKERS=1
+NUM_WORKERS=${NUM_WORKERS:-1}
 
-for ((i=1; i<=NUM_WORKERS; i++)); do
-  # 正确设置环境变量并启动celery worker
-  MACHINE_ID="${i}" /app/.venv/bin/python -m celery -A tasks.celery_app.app worker -Q translate;
-done
+/app/.venv/bin/python -m celery -A tasks.celery_app.app worker -Q translate -c ${NUM_WORKERS};
